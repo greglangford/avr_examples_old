9a6742af1dbc73511a52dbe97c02fa07c909826c
Greg Langford <greg@langford.me>
2017-05-07 20:32:59 +0100

    checkpoint

--- atmega328p_433_tx/main.c
+++ atmega328p_433_tx/main.c
@@ -31,10 +31,10 @@ void main() {
   DDRB = _BV(PB0);
   PORTB &= ~_BV(PB0);
 
-  char a = 0b01010110;
-  char b = 0b00001011;
-  char c = 0b11110011; // socket on
-  char d = 0b01110011; // socket off
+  char a = 0x56; // Socket Identifier Byte 1
+  char b = 0x0B; // Socket Identifier Byte 2
+  char c = 0xF3; // Socket 1 On
+  char d = 0x73; // Socket 1 Off
 
   int i;
 
@@ -44,22 +44,22 @@ void main() {
       tx_byte(a);
       tx_byte(b);
       tx_byte(c);
-      tx_short();
+      tx_short(); // 25 bit seems to always be 0
       _delay_ms(8); // delay between packets
     }
 
     _delay_ms(5000);
 
+
     for(i = 0; i < 6; i++) {
       tx_byte(a);
       tx_byte(b);
       tx_byte(d);
-      tx_short(); // transmit last 0 bit
-      _delay_ms(8);
+      tx_short();
+      _delay_ms(8); // delay between packets
     }
 
     _delay_ms(5000);
-
 
   }
 }
