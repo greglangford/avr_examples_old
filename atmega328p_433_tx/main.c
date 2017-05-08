fba00736fc2e46ecfc58d31b9c3fe2e38206cd80
Greg Langford <greg@langford.me>
2017-05-08 19:01:09 +0100

    doorbell ring

--- atmega328p_433_tx/main.c
+++ atmega328p_433_tx/main.c
@@ -3,17 +3,17 @@
 #include <util/delay.h>
 
 void tx_short() {
+  _delay_us(1000);
   PORTB |= _BV(PB0);
-  _delay_us(375);
+  _delay_us(500);
   PORTB &= ~_BV(PB0);
-  _delay_us(790);
 }
 
 void tx_long() {
+  _delay_us(500);
   PORTB |= _BV(PB0);
-  _delay_us(958);
+  _delay_us(1000);
   PORTB &= ~_BV(PB0);
-  _delay_us(208);
 }
 
 void tx_byte(char c) {
@@ -31,33 +31,24 @@ void main() {
   DDRB = _BV(PB0);
   PORTB &= ~_BV(PB0);
 
-  char a = 0x56; // Socket Identifier Byte 1
-  char b = 0x0B; // Socket Identifier Byte 2
-  char c = 0xF3; // Socket 1 On
-  char d = 0x73; // Socket 1 Off
+  char a = 0b01000110; // Socket Identifier Byte 1
+  char b = 0b10110110; // Socket Identifier Byte 2
 
   int i;
 
   while(1) {
 
-    for(i = 0; i < 6; i++) {
+    for(i = 0; i < 50; i++) {
       tx_byte(a);
       tx_byte(b);
-      tx_byte(c);
       tx_short(); // 25 bit seems to always be 0
-      _delay_ms(8); // delay between packets
+      tx_short(); // 25 bit seems to always be 0
+      tx_short(); // 25 bit seems to always be 0
+      tx_short(); // 25 bit seems to always be 0
+      tx_short(); // 25 bit seems to always be 0
+      _delay_us(2900); // delay between packets
     }
 
-    _delay_ms(5000);
-
-
-    for(i = 0; i < 6; i++) {
-      tx_byte(a);
-      tx_byte(b);
-      tx_byte(d);
-      tx_short();
-      _delay_ms(8); // delay between packets
-    }
 
     _delay_ms(5000);
 
